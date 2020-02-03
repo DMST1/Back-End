725ba0acc6f3d4a6379512c0fc485312511acbe3
Bryan <17adamsbryk@washk12.org>
2020-02-03 12:13:13 -0700

    User Auth and Budget endpoints

---
+++ database/migrations/20200202131554_users.js
@@ -0,0 +1,66 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("users", users => {
+      users.increments();
+      users
+        .string("username", 16)
+        .notNullable()
+        .unique();
+      users.string("password", 24).notNullable();
+    })
+    .createTable("monthlybudget", mbudget => {
+      mbudget.increments();
+      mbudget.integer("Transportation");
+      mbudget.integer("Food");
+      mbudget.integer("HealthInsurance");
+      mbudget.integer("CarInsurance");
+      mbudget.integer("HealthLoans");
+      mbudget.integer("CarLoans");
+      mbudget.integer("PersonalLoans");
+      mbudget.integer("Other");
+      mbudget.string("OtherDescription");
+      mbudget
+        .integer("user_id")
+        .unique()
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("relocatingcost", rcost => {
+      rcost.increments();
+      rcost.integer("DailyHotelCost");
+      rcost.integer("DaysHotelNeeded");
+      rcost.integer("NewRentalDeposit");
+      rcost.integer("UtilityConnection");
+      rcost.integer("StorageUnit");
+      rcost.integer("NewMonthlyRent");
+      rcost.integer("CarRentalCost");
+      rcost.integer("CellDisconnect");
+      rcost.integer("CellConnect");
+      rcost.integer("TruckRental");
+      rcost.integer("GasForTruck");
+      rcost.integer("MentalHealthTreatment");
+      rcost.integer("Other");
+      rcost.string("OtherDescription");
+
+      rcost
+        .integer("user_id")
+        .unique()
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("relocatingcost")
+    .dropTableIfExists("monthlybudget")
+    .dropTableIfExists("users");
+};
---
+++ users/users-model.js
@@ -0,0 +1,65 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  getUserByUsername,
+  addUser,
+  createMonthlyBudget,
+  getMonthlyBudget,
+  updateMonthlyBudget,
+  getUserById,
+  getMonthlyBudgetByID,
+  getMonthlyBudgetTotal
+};
+
+function getUserByUsername(username) {
+  return db("users")
+    .where({ username })
+    .first();
+}
+
+function getUserById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function createMonthlyBudget(budget, id) {
+  return db("monthlybudget")
+    .insert({ ...budget, user_id: id })
+    .then(count => (count > 0 ? this.getMonthlyBudgetByID(id) : null));
+}
+function updateMonthlyBudget(budget, id) {
+  return db("monthlybudget")
+    .where("user_id", id)
+    .update(budget)
+    .then(count => (count > 0 ? this.getMonthlyBudgetByID(id) : null))
+}
+function getMonthlyBudget(username) {
+  return db("users")
+    .join("monthlybudget", "users.id", "monthlybudget.user_id")
+    .where("users.username", username)
+    .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans', 'OtherDescription','Other')
+    .first()
+}
+function getMonthlyBudgetTotal(username) {
+  return db("users")
+    .join("monthlybudget", "users.id", "monthlybudget.user_id")
+    .where("users.username", username)
+    .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans','Other')
+    .first()
+}
+
+
+
+function getMonthlyBudgetByID(id) {
+  return db("users")
+    .join("monthlybudget", "users.id", "monthlybudget.user_id")
+    .where("users.id", id)
+    .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans','Other','OtherDescription','user_id' )
+    .first();
+    
+}
+
+function addUser(user) {
+  return db("users").insert(user);
+} 
