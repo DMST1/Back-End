76ddee325df62e80686d7e67370f8ec4038f19f0
Anisha Sunkerneni <anishasunkerneni@Anishas-MacBook-Air.local>
2020-02-03 12:48:02 -0800

    relocationcosts

--- users/users-model.js
+++ users/users-model.js
@@ -8,7 +8,12 @@ module.exports = {
   updateMonthlyBudget,
   getUserById,
   getMonthlyBudgetByID,
-  getMonthlyBudgetTotal
+  getMonthlyBudgetTotal,
+  createRelocationCost,
+  updateRelocationCost,
+  getRelocationCost,
+  getRelocationCostTotal,
+  getRelocationCostByID
 };
 
 function getUserByUsername(username) {
@@ -49,15 +54,48 @@ function getMonthlyBudgetTotal(username) {
     .first()
 }
 
-
-
 function getMonthlyBudgetByID(id) {
   return db("users")
     .join("monthlybudget", "users.id", "monthlybudget.user_id")
     .where("users.id", id)
     .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans','Other','OtherDescription','user_id' )
-    .first();
-    
+    .first();    
+}
+
+// relocation costs
+
+function createRelocationCost(budget, id) {
+  return db("relocationcost")
+    .insert({ ...budget, user_id: id })
+    .then(count => (count > 0 ? this.getRelocationCostByID(id) : null));
+}
+function updateRelocationCost(budget, id) {
+  return db("relocationcost")
+    .where("user_id", id)
+    .update(budget)
+    .then(count => (count > 0 ? this.getRelocationCostByID(id) : null))
+}
+function getRelocationCost(username) {
+  return db("users")
+    .join("relocationcost", "users.id", "relocationcost.user_id")
+    .where("users.username", username)
+    .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans', 'OtherDescription','Other')
+    .first()
+}
+function getRelocationCostTotal(username) {
+  return db("users")
+    .join("relocationcost", "users.id", "relocationcost.user_id")
+    .where("users.username", username)
+    .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans','Other')
+    .first()
+}
+
+function getRelocationCostByID(id) {
+  return db("users")
+    .join("relocationcost", "users.id", "relocationcost.user_id")
+    .where("users.id", id)
+    .select('Transportation','Food','HealthInsurance','CarInsurance','HealthLoans','CarLoans','PersonalLoans','Other','OtherDescription','user_id' )
+    .first();    
 }
 
 function addUser(user) {
--- users/users-router.js
+++ users/users-router.js
@@ -79,3 +79,70 @@ router.put("/mb", (req, res) => {
     });
 });
 module.exports = router;
+
+//relocation cost
+router.get("/rc", (req, res) => {
+  Users.getRelocationCost(req.user.username).then(rc => {
+    if (rc) {
+      res.status(200).json(rc);
+    } else {
+      res
+        .status(500)
+        .json({
+          message: "This User hasn't filled out their relocation costs yet!"
+        });
+    }
+  });
+});
+
+router.get("/rc/total", (req, res) => {
+  Users.getRelocationCostTotal(req.user.username).then(rc => {
+    if (rc) {
+      const totalValue = obj => Object.values(obj).reduce((a, b) => a + b);
+      console.log(totalValue(rc));
+      res.rcTotal = totalValue(rc)
+      res.status(200).json(totalValue(rc));
+    } else {
+      res
+        .status(500)
+        .json({
+          message: "This User hasn't filled out their relocation costs budget yet"
+        });
+    }
+  });
+});
+router.post("/rc", (req, res) => {
+  Users.getUserByUsername(req.user.username)
+    .then(user => {
+      Users.createRelocationCost(req.body, user.id)
+        .then(response => {
+          res.status(201).json(response);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ message: "Error, do you already have a relocation costs budget?" });
+        });
+    })
+    .catch(err =>
+      res.status(500).json({ message: "This User Probably doesn't exist" })
+    );
+});
+
+router.put("/rc", (req, res) => {
+  Users.getUserByUsername(req.user.username)
+    .then(user => {
+      Users.updateRelocationCost(req.body, user.id)
+        .then(response => {
+          res.status(200).json(response);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ message: "Probably missing required fields" });
+        });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "This User probably doesn't exist" });
+    });
+});
+module.exports = router;
